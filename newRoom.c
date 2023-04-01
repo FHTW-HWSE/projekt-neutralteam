987a94b224489a85129cd3708f4a95a0e0554d0d
NeutralUsername <stuck.fabian@gmail.com>
2023-04-01 21:25:13 +0200

    refactoring

--- newRoom.c
+++ newRoom.c
@@ -2,29 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "menu.h"
+#include "newRoom.h"
+
+
+
 void newRoom() {
     printf("\nnew room\n");
+    
     char str[123] = "0";
-
-    int rows = 0;
+    Room room = {
+        .rows = 0,
+        .cols = 0,
+        .seats = NULL
+    };
     do {
         printf("enter number of rows: ");
         scanf("%s", str);
-        rows = atoi(str);
+        room.rows = atoi(str);
         if (strcmp(str, "-abort") == 0) {
             exit(0);
         }
-    } while(rows < 1);
+    } while(room.rows < 1);
 
-    int cols = 0;
     do {
         printf("enter number of columns: ");
         scanf("%s", str);
-        cols = atoi(str);
+        room.cols = atoi(str);
         if (strcmp(str, "-abort") == 0) {
             exit(0);
         }
-    } while(cols < 1);
+    } while(room.cols < 1);
 
     printf("\nroom layout\n");
     printf("  100%%\t[1]\n");
@@ -41,56 +49,59 @@ void newRoom() {
         }
     } while(layout < 1 || layout > 3);
 
-    char *room[rows][cols];
+    room.seats = (char**) malloc(room.rows * sizeof(char*));
     switch (layout) {
         case 1:
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
-                    room[i][j] = ":";
+            for (int i = 0; i < room.rows; i++) {
+                for (int j = 0; j < room.cols; j++) {
+                    room.seats[i*room.cols+j] = ":";
                 }
             }
+            
             break;
         case 2:
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
+            for (int i = 0; i < room.rows; i++) {
+                for (int j = 0; j < room.cols; j++) {
                     if ( (i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1) ) {
-                        room[i][j] = ":";
+                        room.seats[i*room.cols+j] = ":";
                     } else {
-                        room[i][j] = "";
+                        room.seats[i*room.cols+j] = "";
                     }
                 }
             }
             break;
         case 3:
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
+            for (int i = 0; i < room.rows; i++) {
+                for (int j = 0; j < room.cols; j++) {
                     if (i % 2 == 0 && j % 2 == 0) {
-                       room[i][j] = ":";
+                        room.seats[i*room.cols+j] = ":";
                     } else {
-                        room[i][j] = "";
+                        room.seats[i*room.cols+j] = "";
                     }
                 }
             }
             break;
     }
+
     printf("\nroom created\n");
-    printf("valid seats:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            int index = i*cols+j;
-            if(index == 0) {
-                printf("%d", index);
+    printRoom(room);
+
+
+
+
+}
+
+void printRoom(Room room) {
+      for (int i = 0; i < room.rows; i++) {
+        for (int j = 0; j < room.cols; j++) {
+            printf("%c", '[');
+            if (strcmp(room.seats[i*room.cols+j], ":") == 0) {
+                printf("%c", ' ');
             } else {
-                if (strcmp(room[i][j], ":") == 0) {
-                    printf(", %d", index);
-                }
+                printf("%c", 'X');
             }
+            printf("%c", ']');
         }
+        printf("\n");
     }
-    printf("\n\nenter student id: ");
-    scanf("%s", str);
-    if (strcmp(str, "-abort") == 0) {
-        exit(0);
-    }
-    
 }
--- newRoom.h
+++ newRoom.h
@@ -1,4 +1,10 @@
 #ifndef NEWROOM_H
 #define NEWROOM_H
+typedef struct Room {
+    int rows;
+    int cols;
+    char **seats;
+} Room;
 void newRoom();
+void printRoom(Room room);
 #endif
