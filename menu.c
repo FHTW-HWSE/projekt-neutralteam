1f68f165b8d1ff95ac6da796c36f53ecabfe323b
NeutralUsername <stuck.fabian@gmail.com>
2023-04-01 19:24:40 +0200

    added -abort to quit

--- menu.c
+++ menu.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include "menu.h"
 #include "newRoom.h"
 #include "loadRoom.h"
 
+
 void menu() {
     int option = 0;
-    char str[2] = "0";
+    char str[7] = "0";
+
     do {
-        printf("\nmenu: (exit with -abort)\n");
+        printf("\nmenu: \n");
         printf("  new\t[1]\n");
         printf("  load\t[2]\n");
         printf("select option: ");
+
         scanf("%s", str);
         option = atoi(str);
+
+        if (strcmp(str, "-abort") == 0) {
+            exit(0);
+        }
     }  while(!isValidOption(option));
+
     switch (option) {
         case 1:
             newRoom();
--- newRoom.c
+++ newRoom.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void newRoom() {
     printf("new room\n\n");
     int rows = 0;
-    char str[2] = "0";
+    char str[7] = "0";
     do {
         printf("enter number of rows: ");
         scanf("%s", str);
         rows = atoi(str);
+        if (strcmp(str, "-abort") == 0) {
+            exit(0);
+        }
     } while(rows < 1);
     int cols = 0;
     do {
         printf("enter number of columns: ");
         scanf("%s", str);
         cols = atoi(str);
+        if (strcmp(str, "-abort") == 0) {
+            exit(0);
+        }
     } while(cols < 1);
     printf("rows: %d, cols: %d\n", rows, cols);
 }
