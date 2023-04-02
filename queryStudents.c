c3eaa9d909043e33fb06fd7e1aeaafd5a4627c8a
NeutralUsername <stuck.fabian@gmail.com>
2023-04-02 11:46:03 +0200

    fixed querying

--- queryStudents.c
+++ queryStudents.c
@@ -14,7 +14,9 @@ void printTopLeftSeat(Room *room, int seat) {
     if (row > 0 && col > 0) {
         int topLeftSeat = (row-1)*room->cols + (col-1);
         if (isValidSeat(room, topLeftSeat)) {
-            printf("%s: %d\n", room->seats[topLeftSeat], topLeftSeat);
+            if (strlen(room->seats[topLeftSeat]) > 0) {
+                printf("%s: %d\n", room->seats[topLeftSeat], topLeftSeat);
+            }
         }
     }
 }
@@ -24,7 +26,9 @@ void printTopSeat(Room *room, int seat) {
     if (row > 0) {
         int topSeat = (row-1)*room->cols + col;
         if (isValidSeat(room, topSeat)) {
-            printf("%s: %d\n", room->seats[topSeat], topSeat);
+            if (strlen(room->seats[topSeat]) > 0) {
+                printf("%s: %d\n", room->seats[topSeat], topSeat);
+            }
         }
     }
 }
@@ -34,7 +38,9 @@ void printTopRightSeat(Room *room, int seat) {
     if (row > 0 && col < room->cols-1) {
         int topRightSeat = (row-1)*room->cols + (col+1);
         if (isValidSeat(room, topRightSeat)) {
-            printf("%s: %d\n", room->seats[topRightSeat], topRightSeat);
+            if (strlen(room->seats[topRightSeat]) > 0) {
+                printf("%s: %d\n", room->seats[topRightSeat], topRightSeat);
+            }
         }
     }
 }
@@ -44,7 +50,9 @@ void printRightSeat(Room *room, int seat) {
     if (col < room->cols-1) {
         int rightSeat = row*room->cols + (col+1);
         if (isValidSeat(room, rightSeat)) {
-            printf("%s: %d\n", room->seats[rightSeat], rightSeat);
+            if (strlen(room->seats[rightSeat]) > 0) {
+                printf("%s: %d\n", room->seats[rightSeat], rightSeat);
+            }
         }
     }
 }
@@ -54,7 +62,9 @@ void printBottomRightSeat(Room *room, int seat) {
     if (row < room->rows-1 && col < room->cols-1) {
         int bottomRightSeat = (row+1)*room->cols + (col+1);
         if (isValidSeat(room, bottomRightSeat)) {
-            printf("%s: %d\n", room->seats[bottomRightSeat], bottomRightSeat);
+            if (strlen(room->seats[bottomRightSeat]) > 0) {
+                printf("%s: %d\n", room->seats[bottomRightSeat], bottomRightSeat);
+            }
         }
     }
 }
@@ -64,7 +74,9 @@ void printBottomSeat(Room *room, int seat) {
     if (row < room->rows-1) {
         int bottomSeat = (row+1)*room->cols + col;
         if (isValidSeat(room, bottomSeat)) {
-            printf("%s: %d\n", room->seats[bottomSeat], bottomSeat);
+            if (strlen(room->seats[bottomSeat]) > 0) {
+                printf("%s: %d\n", room->seats[bottomSeat], bottomSeat);
+            }
         }
     }
 }
@@ -74,7 +86,9 @@ void printBottomLeftSeat(Room *room, int seat) {
     if (row < room->rows-1 && col > 0) {
         int bottomLeftSeat = (row+1)*room->cols + (col-1);
         if (isValidSeat(room, bottomLeftSeat)) {
-            printf("%s: %d\n", room->seats[bottomLeftSeat], bottomLeftSeat);
+            if (strlen(room->seats[bottomLeftSeat]) > 0) {
+                printf("%s: %d\n", room->seats[bottomLeftSeat], bottomLeftSeat);
+            }
         }
     }
 }
@@ -84,172 +98,218 @@ void printLeftSeat(Room *room, int seat) {
     if (col > 0) {
         int leftSeat = row*room->cols + (col-1);
         if (isValidSeat(room, leftSeat)) {
-            printf("%s: %d\n", room->seats[leftSeat], leftSeat);
+            if (strlen(room->seats[leftSeat]) > 0) {
+                printf("%s: %d\n", room->seats[leftSeat], leftSeat);
+            }
         }
     }
 }
 
 
-void printIndirectTopLeftSeat(Room *room, int seat) {
+
+
+
+
+
+
+
+
+void printTopTopLeftLeftSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row > 1 && col > 1) {
-        int topLeftSeat = (row-2)*room->cols + (col-2);
-        if (isValidSeat(room, topLeftSeat)) {
-            printf("%s: %d\n", room->seats[topLeftSeat], topLeftSeat);
+        int topTopLeftLeftSeat = (row-2)*room->cols + (col-2);
+        if (isValidSeat(room, topTopLeftLeftSeat)) {
+            if (strlen(room->seats[topTopLeftLeftSeat]) > 0) {
+                printf("%s: %d\n", room->seats[topTopLeftLeftSeat], topTopLeftLeftSeat);
+            }
         }
     }
 }
-void printIndirectTopTopLeftSeat(Room *room, int seat) {
+void printTopTopLeftSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row > 1 && col > 0) {
         int topTopLeftSeat = (row-2)*room->cols + (col-1);
         if (isValidSeat(room, topTopLeftSeat)) {
-            printf("%s: %d\n", room->seats[topTopLeftSeat], topTopLeftSeat);
+            if (strlen(room->seats[topTopLeftSeat]) > 0) {
+                printf("%s: %d\n", room->seats[topTopLeftSeat], topTopLeftSeat);
+            }
         }
     }
 }
-void printIndirectTopSeat(Room *room, int seat) {
+void printTopTopSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row > 1) {
-        int topSeat = (row-2)*room->cols + col;
-        if (isValidSeat(room, topSeat)) {
-            printf("%s: %d\n", room->seats[topSeat], topSeat);
+        int topTopSeat = (row-2)*room->cols + col;
+        if (isValidSeat(room, topTopSeat)) {
+            if (strlen(room->seats[topTopSeat]) > 0) {
+                printf("%s: %d\n", room->seats[topTopSeat], topTopSeat);
+            }
         }
     }
 }
-void printIndirectTopTopRightSeat(Room *room, int seat) {
+void printTopTopRightSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row > 1 && col < room->cols-1) {
         int topTopRightSeat = (row-2)*room->cols + (col+1);
         if (isValidSeat(room, topTopRightSeat)) {
-            printf("%s: %d\n", room->seats[topTopRightSeat], topTopRightSeat);
+            if (strlen(room->seats[topTopRightSeat]) > 0) {
+                printf("%s: %d\n", room->seats[topTopRightSeat], topTopRightSeat);
+            }
         }
     }
 }
-void printIndirectTopRightSeat(Room *room, int seat) {
+void printTopTopRightRightSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row > 1 && col < room->cols-2) {
-        int topRightSeat = (row-2)*room->cols + (col+2);
-        if (isValidSeat(room, topRightSeat)) {
-            printf("%s: %d\n", room->seats[topRightSeat], topRightSeat);
+        int topTopRightRightSeat = (row-2)*room->cols + (col+2);
+        if (isValidSeat(room, topTopRightRightSeat)) {
+            if (strlen(room->seats[topTopRightRightSeat]) > 0) {
+                printf("%s: %d\n", room->seats[topTopRightRightSeat], topTopRightRightSeat);
+            }
         }
     }
 }
-void printIndirectRightRightTopSeat(Room *room, int seat) {
+void printTopRightRightSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row > 0 && col < room->cols-2) {
-        int rightRightTopSeat = (row-1)*room->cols + (col+2);
-        if (isValidSeat(room, rightRightTopSeat)) {
-            printf("%s: %d\n", room->seats[rightRightTopSeat], rightRightTopSeat);
+        int topRightRightSeat = (row-1)*room->cols + (col+2);
+        if (isValidSeat(room, topRightRightSeat)) {
+            if (strlen(room->seats[topRightRightSeat]) > 0) {
+                printf("%s: %d\n", room->seats[topRightRightSeat], topRightRightSeat);
+            }
         }
     }
 }
-void printIndirectRightSeat(Room *room, int seat) {
+void printRightRightSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (col < room->cols-2) {
-        int rightSeat = row*room->cols + (col+2);
-        if (isValidSeat(room, rightSeat)) {
-            printf("%s: %d\n", room->seats[rightSeat], rightSeat);
+        int rightRightSeat = row*room->cols + (col+2);
+        if (isValidSeat(room, rightRightSeat)) {
+            if (strlen(room->seats[rightRightSeat]) > 0) {
+                printf("%s: %d\n", room->seats[rightRightSeat], rightRightSeat);
+            }
         }
     }
 }
-void printIndirectRightRightBottomSeat(Room *room, int seat) {
+void printBottomRightRightSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row < room->rows-1 && col < room->cols-2) {
-        int rightRightBottomSeat = (row+1)*room->cols + (col+2);
-        if (isValidSeat(room, rightRightBottomSeat)) {
-            printf("%s: %d\n", room->seats[rightRightBottomSeat], rightRightBottomSeat);
+        int bottomRightRightSeat = (row+1)*room->cols + (col+2);
+        if (isValidSeat(room, bottomRightRightSeat)) {
+            if (strlen(room->seats[bottomRightRightSeat]) > 0) {
+                printf("%s: %d\n", room->seats[bottomRightRightSeat], bottomRightRightSeat);
+            }
         }
     }
 }
-void printIndirectBottomRightSeat(Room *room, int seat) {
+void printBottomBottomRightRightSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row < room->rows-2 && col < room->cols-2) {
-        int bottomRightSeat = (row+2)*room->cols + (col+2);
-        if (isValidSeat(room, bottomRightSeat)) {
-            printf("%s: %d\n", room->seats[bottomRightSeat], bottomRightSeat);
+        int bottomBottomRightRightSeat = (row+2)*room->cols + (col+2);
+        if (isValidSeat(room, bottomBottomRightRightSeat)) {
+            if (strlen(room->seats[bottomBottomRightRightSeat]) > 0) {
+                printf("%s: %d\n", room->seats[bottomBottomRightRightSeat], bottomBottomRightRightSeat);
+            }
         }
     }
 }
-void printIndirectBottomBottomRightSeat(Room *room, int seat) {
+void printBottomBottomRightSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row < room->rows-2 && col < room->cols-1) {
         int bottomBottomRightSeat = (row+2)*room->cols + (col+1);
         if (isValidSeat(room, bottomBottomRightSeat)) {
-            printf("%s: %d\n", room->seats[bottomBottomRightSeat], bottomBottomRightSeat);
+            if (strlen(room->seats[bottomBottomRightSeat]) > 0) {
+                printf("%s: %d\n", room->seats[bottomBottomRightSeat], bottomBottomRightSeat);
+            }
         }
     }
 }
-void printIndirectBottomSeat(Room *room, int seat) {
+void printBottomBottomSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row < room->rows-2) {
-        int bottomSeat = (row+2)*room->cols + col;
-        if (isValidSeat(room, bottomSeat)) {
-            printf("%s: %d\n", room->seats[bottomSeat], bottomSeat);
+        int bottomBottomSeat = (row+2)*room->cols + col;
+        if (isValidSeat(room, bottomBottomSeat)) {
+            if (strlen(room->seats[bottomBottomSeat]) > 0) {
+                printf("%s: %d\n", room->seats[bottomBottomSeat], bottomBottomSeat);
+            }
         }
     }
 }
-void printIndirectBottomBottomLeftSeat(Room *room, int seat) {
+void printBottomBottomLeftSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row < room->rows-2 && col > 0) {
         int bottomBottomLeftSeat = (row+2)*room->cols + (col-1);
         if (isValidSeat(room, bottomBottomLeftSeat)) {
-            printf("%s: %d\n", room->seats[bottomBottomLeftSeat], bottomBottomLeftSeat);
+            if (strlen(room->seats[bottomBottomLeftSeat]) > 0) {
+                printf("%s: %d\n", room->seats[bottomBottomLeftSeat], bottomBottomLeftSeat);
+            }
         }
     }
 }
-void printIndirectBottomLeftSeat(Room *room, int seat) {
+void printBottomBottomLeftLeftSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row < room->rows-2 && col > 1) {
-        int bottomLeftSeat = (row+2)*room->cols + (col-2);
-        if (isValidSeat(room, bottomLeftSeat)) {
-            printf("%s: %d\n", room->seats[bottomLeftSeat], bottomLeftSeat);
+        int bottomBottomLeftLeftSeat = (row+2)*room->cols + (col-2);
+        if (isValidSeat(room, bottomBottomLeftLeftSeat)) {
+            if (strlen(room->seats[bottomBottomLeftLeftSeat]) > 0) {
+                printf("%s: %d\n", room->seats[bottomBottomLeftLeftSeat], bottomBottomLeftLeftSeat);
+            }
         }
     }
 }
-void printIndirectLeftLeftBottomSeat(Room *room, int seat) {
+void printBottomLeftLeftSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row < room->rows-1 && col > 1) {
-        int leftLeftBottomSeat = (row+1)*room->cols + (col-2);
-        if (isValidSeat(room, leftLeftBottomSeat)) {
-            printf("%s: %d\n", room->seats[leftLeftBottomSeat], leftLeftBottomSeat);
+        int bottomLeftLeftSeat = (row+1)*room->cols + (col-2);
+        if (isValidSeat(room, bottomLeftLeftSeat)) {
+            if (strlen(room->seats[bottomLeftLeftSeat]) > 0) {
+                printf("%s: %d\n", room->seats[bottomLeftLeftSeat], bottomLeftLeftSeat);
+            }
         }
     }
 }
-void printIndirectLeftSeat(Room *room, int seat) {
+void printLeftLeftSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (col > 1) {
-        int leftSeat = row*room->cols + (col-2);
-        if (isValidSeat(room, leftSeat)) {
-            printf("%s: %d\n", room->seats[leftSeat], leftSeat);
+        int leftLeftSeat = row*room->cols + (col-2);
+        if (isValidSeat(room, leftLeftSeat)) {
+            if (strlen(room->seats[leftLeftSeat]) > 0) {
+                printf("%s: %d\n", room->seats[leftLeftSeat], leftLeftSeat);
+            }
         }
     }
 }
-void printIndirectLeftLeftTopSeat(Room *room, int seat) {
+void printTopLeftLeftSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
     if (row > 0 && col > 1) {
-        int leftLeftTopSeat = (row-1)*room->cols + (col-2);
-        if (isValidSeat(room, leftLeftTopSeat)) {
-            printf("%s: %d\n", room->seats[leftLeftTopSeat], leftLeftTopSeat);
+        int topLeftLeftSeat = (row-1)*room->cols + (col-2);
+        if (isValidSeat(room, topLeftLeftSeat)) {
+            if (strlen(room->seats[topLeftLeftSeat]) > 0) {
+                printf("%s: %d\n", room->seats[topLeftLeftSeat], topLeftLeftSeat);
+            }
         }
     }
 }
+
+
+
+
 
 
 
@@ -298,22 +358,22 @@ void queryStudents(Room *room) {
         printLeftSeat(room, seat);
 
         printf("\nindirect neighbors: \n");
-        printIndirectTopLeftSeat(room, seat);
-        printIndirectTopTopLeftSeat(room, seat);
-        printIndirectTopSeat(room, seat);
-        printIndirectTopTopRightSeat(room, seat);
-        printIndirectTopRightSeat(room, seat);
-        printIndirectRightRightTopSeat(room, seat);
-        printIndirectRightSeat(room, seat);
-        printIndirectRightRightBottomSeat(room, seat);
-        printIndirectBottomRightSeat(room, seat);
-        printIndirectBottomBottomRightSeat(room, seat);
-        printIndirectBottomSeat(room, seat);
-        printIndirectBottomBottomLeftSeat(room, seat);
-        printIndirectBottomLeftSeat(room, seat);
-        printIndirectLeftLeftBottomSeat(room, seat);
-        printIndirectLeftSeat(room, seat);
-        printIndirectLeftLeftTopSeat(room, seat);
+        printTopTopLeftLeftSeat(room, seat);
+        printTopTopLeftSeat(room, seat);
+        printTopTopSeat(room, seat);
+        printTopTopRightSeat(room, seat);
+        printTopTopRightRightSeat(room, seat);
+        printTopRightRightSeat(room, seat);
+        printRightRightSeat(room, seat);
+        printBottomRightRightSeat(room, seat);
+        printBottomBottomRightRightSeat(room, seat);
+        printBottomBottomRightSeat(room, seat);
+        printBottomBottomSeat(room, seat);
+        printBottomBottomLeftSeat(room, seat);
+        printBottomBottomLeftLeftSeat(room, seat);
+        printBottomLeftLeftSeat(room, seat);
+        printLeftLeftSeat(room, seat);
+        printTopLeftLeftSeat(room, seat);
     }
     freeRoom(room);
 }
