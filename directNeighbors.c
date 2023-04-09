bd9b259fd6e6b36de5a6960d58810d0b942a492f
NeutralUsername <stuck.fabian@gmail.com>
2023-04-09 11:33:50 +0200

    refactoring

--- directNeighbors.c
+++ directNeighbors.c
@@ -6,64 +6,40 @@
 int getTopLeftSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
-    if (row > 0 && col > 0) {
-        return (row-1)*room->cols + (col-1);
-    }
-    return -1;
+    return row > 0 && col > 0 ? (row-1)*room->cols + (col-1) : -1;
 }
 int getTopSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
-    if (row > 0) {
-        return (row-1)*room->cols + col;
-    }
-    return -1;
+    return row > 0 ? (row-1)*room->cols + col : -1;
 }
 int getTopRightSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
-    if (row > 0 && col < room->cols-1) {
-        return (row-1)*room->cols + (col+1);
-    }
-    return -1;
+    return row > 0 && col < room->cols-1 ? (row-1)*room->cols + (col+1) : -1;
 }
 int getRightSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
-    if (col < room->cols-1) {
-        return row*room->cols + (col+1);
-    }
-    return -1;
+    return col < room->cols-1 ? row*room->cols + (col+1) : -1;
 }
 int getBottomRightSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
-    if (row < room->rows-1 && col < room->cols-1) {
-        return (row+1)*room->cols + (col+1);
-    }
-    return -1;
+    return row < room->rows-1 && col < room->cols-1 ? (row+1)*room->cols + (col+1) : -1;
 }
 int getBottomSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
-    if (row < room->rows-1) {
-        return (row+1)*room->cols + col;
-    }
-    return -1;
+    return row < room->rows-1 ? (row+1)*room->cols + col : -1;
 }
 int getBottomLeftSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
-    if (row < room->rows-1 && col > 0) {
-        return (row+1)*room->cols + (col-1);
-    }
-    return -1;
+    return row < room->rows-1 && col > 0 ? (row+1)*room->cols + (col-1) : -1;
 }
 int getLeftSeat(Room *room, int seat) {
     int row = seat / room->cols;
     int col = seat % room->cols;
-    if (col > 0) {
-        return row*room->cols + (col-1);
-    }
-    return -1;
+    return col > 0 ? row*room->cols + (col-1) : -1;
 }
