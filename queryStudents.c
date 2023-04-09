7aff56197f594a094676fa80d70503ecb9e421fe
NeutralUsername <stuck.fabian@gmail.com>
2023-04-09 10:20:58 +0200

    refactoring

--- queryStudents.c
+++ queryStudents.c
@@ -35,118 +35,39 @@ void queryStudents(Room *room) {
 
 void printNeighbors(Room *room, int seat) {
     printf("\ndirect neighbors: \n");
-    int topLeftSeat = getTopLeftSeat(room, seat);
-    if (topLeftSeat != -1) {
-        if (strlen(room->seats[topLeftSeat]) > 0)
-            printf("%d: %s\n", topLeftSeat, room->seats[topLeftSeat]);
-    }
-    int topSeat = getTopSeat(room, seat);
-    if (topSeat != -1) {
-        if (strlen(room->seats[topSeat]) > 0)
-            printf("%d: %s\n", topSeat, room->seats[topSeat]);
-    }
-    int topRightSeat = getTopRightSeat(room, seat);
-    if (topRightSeat != -1) {
-        if (strlen(room->seats[topRightSeat]) > 0)
-            printf("%d: %s\n", topRightSeat, room->seats[topRightSeat]);
-    }
-    int rightSeat = getRightSeat(room, seat);
-    if (rightSeat != -1) {
-        if (strlen(room->seats[rightSeat]) > 0)
-            printf("%d: %s\n", rightSeat, room->seats[rightSeat]);
-    }
-    int bottomRightSeat = getBottomRightSeat(room, seat);
-    if (bottomRightSeat != -1) {
-        if (strlen(room->seats[bottomRightSeat]) > 0)
-            printf("%d: %s\n", bottomRightSeat, room->seats[bottomRightSeat]);
-    }
-    int bottomSeat = getBottomSeat(room, seat);
-    if (bottomSeat != -1) {
-        if (strlen(room->seats[bottomSeat]) > 0)
-            printf("%d: %s\n", bottomSeat, room->seats[bottomSeat]);
-    }
-    int bottomLeftSeat = getBottomLeftSeat(room, seat);
-    if (bottomLeftSeat != -1) {
-        if (strlen(room->seats[bottomLeftSeat]) > 0)
-            printf("%d: %s\n", bottomLeftSeat, room->seats[bottomLeftSeat]);
-    }
-    int leftSeat = getLeftSeat(room, seat);
-    if (leftSeat != -1) {
-        if (strlen(room->seats[leftSeat]) > 0)
-            printf("%d: %s\n", leftSeat, room->seats[leftSeat]);
-    }
+    printSeat(room, getTopLeftSeat(room, seat));
+    printSeat(room, getTopSeat(room, seat));
+    printSeat(room, getTopRightSeat(room, seat));
+    printSeat(room, getRightSeat(room, seat));
+    printSeat(room, getBottomRightSeat(room, seat));
+    printSeat(room, getBottomSeat(room, seat));
+    printSeat(room, getBottomLeftSeat(room, seat));
+    printSeat(room, getLeftSeat(room, seat));
     printf("\nindirect neighbors: \n");
-    int TopTopLeftLeftSeat = getTopTopLeftLeftSeat(room, seat);
-    if (TopTopLeftLeftSeat != -1) {
-        if (strlen(room->seats[TopTopLeftLeftSeat]) > 0)
-            printf("%d: %s\n", TopTopLeftLeftSeat, room->seats[TopTopLeftLeftSeat]);
+    printSeat(room, getTopTopLeftLeftSeat(room, seat));
+    printSeat(room, getTopTopLeftSeat(room, seat));
+    printSeat(room, getTopTopSeat(room, seat));
+    printSeat(room, getTopTopRightSeat(room, seat));
+    printSeat(room, getTopTopRightRightSeat(room, seat));
+    printSeat(room, getTopLeftLeftSeat(room, seat));
+    printSeat(room, getTopRightRightSeat(room, seat));
+    printSeat(room, getBottomBottomLeftLeftSeat(room, seat));
+    printSeat(room, getBottomBottomLeftSeat(room, seat));
+    printSeat(room, getBottomBottomSeat(room, seat));
+    printSeat(room, getBottomBottomRightSeat(room, seat));
+    printSeat(room, getBottomBottomRightRightSeat(room, seat));
+    printSeat(room, getBottomLeftLeftSeat(room, seat));
+    printSeat(room, getBottomRightRightSeat(room, seat));
+    printSeat(room, getLeftLeftSeat(room, seat));
+    printSeat(room, getRightRightSeat(room, seat));
+}
+
+void printSeat(Room *room, int seat) {
+    if (isValidSeat(room, seat)) {
+        if (strlen(room->seats[seat]) > 0) {
+            printf("%d: %s\n", seat, room->seats[seat]);
+        }
     }
-    int TopTopLeftSeat = getTopTopLeftSeat(room, seat);
-    if (TopTopLeftSeat != -1) {
-        if (strlen(room->seats[TopTopLeftSeat]) > 0)
-            printf("%d: %s\n", TopTopLeftSeat, room->seats[TopTopLeftSeat]);
-    }
-    int TopTopSeat = getTopTopSeat(room, seat);
-    if (TopTopSeat != -1) {
-        if (strlen(room->seats[TopTopSeat]) > 0)
-            printf("%d: %s\n", TopTopSeat, room->seats[TopTopSeat]);
-    }
-    int TopTopRightSeat = getTopTopRightSeat(room, seat);
-    if (TopTopRightSeat != -1) {
-        if (strlen(room->seats[TopTopRightSeat]) > 0)
-            printf("%d: %s\n", TopTopRightSeat, room->seats[TopTopRightSeat]);
-    }
-    int TopTopRightRightSeat = getTopTopRightRightSeat(room, seat);
-    if (TopTopRightRightSeat != -1) {
-        if (strlen(room->seats[TopTopRightRightSeat]) > 0)
-            printf("%d: %s\n", TopTopRightRightSeat, room->seats[TopTopRightRightSeat]);
-    }
-    int TopRightRightSeat = getTopRightRightSeat(room, seat);
-    if (TopRightRightSeat != -1) {
-        if (strlen(room->seats[TopRightRightSeat]) > 0)
-            printf("%d: %s\n", TopRightRightSeat, room->seats[TopRightRightSeat]);
-    }
-    int RightRightSeat = getRightRightSeat(room, seat);
-    if (RightRightSeat != -1) {
-        if (strlen(room->seats[RightRightSeat]) > 0)
-            printf("%d: %s\n", RightRightSeat, room->seats[RightRightSeat]);
-    }
-    int BottomBottomRightRightSeat = getBottomBottomRightRightSeat(room, seat);
-    if (BottomBottomRightRightSeat != -1) {
-        if (strlen(room->seats[BottomBottomRightRightSeat]) > 0)
-            printf("%d: %s\n", BottomBottomRightRightSeat, room->seats[BottomBottomRightRightSeat]);
-    }
-    int BottomBottomRightSeat = getBottomBottomRightSeat(room, seat);
-    if (BottomBottomRightSeat != -1) {
-        if (strlen(room->seats[BottomBottomRightSeat]) > 0)
-            printf("%d: %s\n", BottomBottomRightSeat, room->seats[BottomBottomRightSeat]);
-    }
-    int BottomBottomSeat = getBottomBottomSeat(room, seat);
-    if (BottomBottomSeat != -1) {
-        if (strlen(room->seats[BottomBottomSeat]) > 0)
-            printf("%d: %s\n", BottomBottomSeat, room->seats[BottomBottomSeat]);
-    }
-    int BottomBottomLeftSeat = getBottomBottomLeftSeat(room, seat);
-    if (BottomBottomLeftSeat != -1) {
-        if (strlen(room->seats[BottomBottomLeftSeat]) > 0)
-            printf("%d: %s\n", BottomBottomLeftSeat, room->seats[BottomBottomLeftSeat]);
-    }
-    int BottomBottomLeftLeftSeat = getBottomBottomLeftLeftSeat(room, seat);
-    if (BottomBottomLeftLeftSeat != -1) {
-        if (strlen(room->seats[BottomBottomLeftLeftSeat]) > 0)
-            printf("%d: %s\n", BottomBottomLeftLeftSeat, room->seats[BottomBottomLeftLeftSeat]);
-    }
-    int BottomLeftLeftSeat = getBottomLeftLeftSeat(room, seat);
-    if (BottomLeftLeftSeat != -1) {
-        if (strlen(room->seats[BottomLeftLeftSeat]) > 0)
-            printf("%d: %s\n", BottomLeftLeftSeat, room->seats[BottomLeftLeftSeat]);
-    }
-    int LeftLeftSeat = getLeftLeftSeat(room, seat);
-    if (LeftLeftSeat != -1) {
-        if (strlen(room->seats[LeftLeftSeat]) > 0)
-            printf("%d: %s\n", LeftLeftSeat, room->seats[LeftLeftSeat]);
-    }
-    printf("\n");
 }
   
 int getSeatByStudentId(Room *room, char *studentId) {
--- queryStudents.h
+++ queryStudents.h
@@ -4,4 +4,5 @@
 void queryStudents(Room *room);
 int getSeatByStudentId(Room *room, char *studentId);
 void printNeighbors(Room *room, int seat);
+void printSeat(Room *room, int seat);
 #endif
